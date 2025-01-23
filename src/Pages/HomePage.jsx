770c419b84661ef780002ffb0c332ae30a5c35ac
Aftab-truwix <aftaba@truwix.com>
2025-01-23 17:49:56 +0530

    Hero section done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,15 +1,34 @@
-
-import NavBar from '../Components/Navbar.jsx'
+import NavBar from "../Components/Navbar.jsx";
+import hero from "../assets/hero.jpg";
 
 const HomePage = () => {
   return (
-    <div className='w-full h-screen relative'>
+    <div
+      className="w-full h-screen "
+      style={{
+        backgroundImage: `url(${hero})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       <NavBar />
-      <div>
-        <img src="" alt=""/>
+      <div className="absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 text-center flex flex-col gap-6">
+        <h1 className="text-6xl font-bold text-white bg-clip-text text-transparent bg-gradient-to-b from-[#FFFFFF] to-[#E1BBFB]">
+          Where Creativity
+          <br />
+          Meets Purpose
+        </h1>{" "}
+        <p className="text-white text-sm">
+          Creative design to build a brand <br />
+          that’s strong, impactful, and full of meaning.
+        </p>
+        <div className="flex gap-10">
+            <button className="w-[200px] py-3 bg-white text-black rounded-xl cursor-pointer">See Our Work</button>
+            <button className="w-[200px] py-3 bg-black text-white rounded-xl cursor-pointer">Get A Quote</button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
