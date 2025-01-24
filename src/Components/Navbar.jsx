b54f1b4911954c0b0cfdacdaae58696c235a7ac0
Aftab-truwix <aftaba@truwix.com>
2025-01-24 14:52:11 +0530

    Conatact page Done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,9 @@ const Navbar = () => {
     <>
       <div className="w-[90%] relative mx-auto flex flex-col bg-gradient-to-b from-[#0e0811] to-[#1e213d] rounded-2xl">
         <div className="w-full h-auto flex justify-between items-center p-2 rounded-2xl xl:px-8 relative z-50">
+          <Link to="/">
           <img src={Logo} alt="Truwix Logo" className="w-36" />
+          </Link>
           <div className="hidden md:flex w-auto h-auto gap-6 xl:gap-8 text-white xl:text-lg">
             <span>Home</span>
             <span>Why</span>
